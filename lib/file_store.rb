ffd1b9d60969272a2f113bde542c8fe0dbee4784
Simon Kiener <jugglinghobo@gmail.com>
2016-04-22 18:43:44 +0200

    fixed list action if list is empty

--- lib/file_store.rb
+++ lib/file_store.rb
@@ -5,9 +5,10 @@ module Kate
     FILE_PATH = "#{HOME}/list.yml"
 
     def self.load(options = {})
-      YAML.load_file(FILE_PATH).each do |hash|
-        hash.keys.each do
-          |key| hash[key.to_sym] = hash.delete(key)
+      items = YAML.load_file(FILE_PATH)
+      (items || []).each do |hash|
+        hash.keys.each do |key|
+          hash[key.to_sym] = hash.delete(key)
         end
       end
     end
